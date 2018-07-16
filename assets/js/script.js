07d645be3f04a87b98e0c80513403a80f95cf828
DragonVirus <limonster.li@gmail.com>
2018-07-17 03:05:22 +0630

    Init commit

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+var app = new Vue({
+  el: '#martyr-app',
+  data: {
+    martyrs : [
+    	{
+    		name : 'ဗိုလ်ချုပ်အောင်ဆန်း',
+    		image : 'assets/images/Aung_San.jpg',
+    		link : 'aung_san.html'
+    	},
+      {
+        name : 'ဒီးဒုတ်ဦးဘချို',
+        image : 'assets/images/UBaCho.jpg',
+        link : '#'
+      },
+      {
+        name : 'မန်းဘခိုင်',
+        image : 'assets/images/mann_ba_khine.jpg',
+        link : '#'
+      },
+      {
+        name : 'ဦးဘဝင်း',
+        image : 'assets/images/u_ba_win.jpg',
+        link : '#'
+      },
+      {
+        name : 'သခင်မြ',
+        image : 'assets/images/thakhin_mya.jpg',
+        link : '#'
+      },
+      {
+        name : 'ဦးအဗ္ဗဒူရာဇတ်',
+        image : 'assets/images/razak.jpg',
+        link : '#'
+      },
+      {
+        name : 'စဝ်စံထွန်း',
+        image : 'assets/images/sao_san_tun.jpg',
+        link : '#'
+      },
+      {
+        name : 'ဦးအုန်းမောင်',
+        image : 'assets/images/ohn_maung.jpg',
+        link : '#'
+      },
+      {
+        name : 'ရဲဘော် ကိုထွေး',
+        image : 'assets/images/ko_htwe.jpg',
+        link : '#'
+      }
+    ]
+  }
+});
